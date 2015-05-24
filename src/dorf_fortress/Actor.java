8e337f031316bdbc8cc8b80c09ac9af740b4ece5
Joe Adkisson <adkissonj@carleton.edu>
2015-05-24 17:32:04 -0500

    Worked on some more inheritance stuff

--- src/dorf_fortress/Actor.java
+++ src/dorf_fortress/Actor.java
@@ -7,4 +7,13 @@ import javafx.scene.Node;
  * Superclass that contains both the controllable character and enemy objects.
  */
 public class Actor extends Sprite {
+    public String name = "";
+
+    public void step() {
+        //do stuff
+    }
+
+    public void die() {
+        //do stuff
+    }
 }
---
+++ src/dorf_fortress/Sprite.java
@@ -0,0 +1,13 @@
+package dorf_fortress;
+
+import javafx.scene.image.ImageView;
+
+/**
+ * Created by Joe Adkisson on 5/24/2015.
+ * A sprite class; it's the superclass for all visible objects in the gameworld.
+ * (With the probable exception of our background image.
+ */
+public class Sprite extends ImageView {
+    //hitbox stuff. That's really all we need to add here.
+    //but look up the ImageView stuff.
+}
