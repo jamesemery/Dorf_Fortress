62670f0f53ac13aaec718e94e20861d96a7afe9e
jamie <emeryj@carleton.edu>
2015-05-27 14:51:14 -0500

    Created an input buffer set for usage later for every frame

---
+++ src/dorf_fortress/InputBuffer.java
@@ -0,0 +1,30 @@
+package dorf_fortress;
+
+
+import java.util.Set;
+import java.util.HashSet;
+
+/**
+ * Class that stores input that is entered during the Main simulation loop.
+ * It is able to be reset so the input is clean for each new frame. This
+ * allows classes elsewhere in main to mimic user input by entering it into
+ * this buffer itself so it will excecute as though the user inputted it.
+ *
+ * Created by jamie on 5/27/15.
+ */
+public class InputBuffer {
+    private Set<String> storedInput = new HashSet<String>();
+
+
+    public void addInput(String input){
+        storedInput.add(input);
+    }
+
+    public boolean getInput(String input){
+        return storedInput.contains(input);
+    }
+
+    public void reset(){
+        storedInput.clear();
+    }
+}
