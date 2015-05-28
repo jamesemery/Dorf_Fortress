58287c6dea395b1e8f12777467278a576cc2650a
jamie <emeryj@carleton.edu>
2015-05-27 22:29:27 -0500

    Unhooked the code from a dependance on the menue classes because it wasn't working

--- src/dorf_fortress/Model.java
+++ src/dorf_fortress/Model.java
@@ -1,13 +1,17 @@
 package dorf_fortress;
 
+
+import java.util.List;
 /**
  * Created by jamie on 5/27/15.
  */
 public class Model {
     List<Entity> entities;
 
+
     public Model() {
 
     }
 
 }
+
