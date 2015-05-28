984e920963919c2a85699c4b2d98f3fdf7f98ab4
Michael Stoneman <azureillusions@eduroam-171-85.dyn.carleton.edu>
2015-05-27 22:01:13 -0500

    so confufsed, brain hurts, want to cry

--- src/dorf_fortress/Main.java
+++ src/dorf_fortress/Main.java
@@ -25,6 +25,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Dorf Fortress");
+        primaryStage.setResizable(false);
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("mainmenu.fxml"));
         Parent menuRoot = (Parent) loader.load();
         final MainMenuController mainMenuController = loader.getController();
@@ -39,53 +42,43 @@ public class Main extends Application {
             }
         });
 
-//        root.setOnKeyPressed(new EventHandler<KeyEvent>() {
-//            @Override
-//            public void handle(KeyEvent keyEvent) {
-//                controller.handleKeyPress(keyEvent);
-//            }
-//        });
 
 
+        //Example code for loading from an fxml file. We might need this for
+        //the main menu?
+        //FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //In the meantime, we'll just do this...
+        Group root = new Group();
+        //...and fill in the objects by hand. That's much nicer to do for our
+        //actual platformer.
 
 
-
-//        //Example code for loading from an fxml file. We might need this for
-//        //the main menu?
-//        //FXMLLoader.load(getClass().getResource("sample.fxml"));
-//        //In the meantime, we'll just do this...
-//        Group root = new Group();
-//        //...and fill in the objects by hand. That's much nicer to do for our
-//        //actual platformer.
-
-        primaryStage.setTitle("Dorf Fortress");
-        primaryStage.setResizable(false);
         Scene platformerBasics = new Scene(menuRoot, SCENE_WIDTH, SCENE_HEIGHT);
 
-//        //Make a tiled background.
-//        BackgroundImage myBI = new BackgroundImage(
-//                new Image("sprites/BasicTile.png"),
-//                BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT,
-//                BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
-//        Region platformerBasicsRegion = new Region();
-//        platformerBasicsRegion.setPrefSize(SCENE_WIDTH+20, SCENE_HEIGHT+20);
-//        platformerBasicsRegion.setBackground(new Background(myBI));
-//        root.getChildren().add(platformerBasicsRegion);
-//
-//        //Make a Dorf!
-//        Dorf ferdinand = new Dorf("sprites/BasicDorf.png", 32, 32, "Ferdinand");
-//        ferdinand.setX(34);
-//        ferdinand.setY(100);
-//        root.getChildren().add(ferdinand.getSprite());
-//
-//        //Set up the controller. (Hopefully)
-//        GameController controller = new GameController(ferdinand);
-//        controller.initialize();
-//
-//        // Set up a KeyEvent handler so we can respond to keyboard activity.
-//        root.setOnKeyPressed(controller);
-//        root.requestFocus();
-//
+        //Make a tiled background.
+        BackgroundImage myBI = new BackgroundImage(
+                new Image("sprites/BasicTile.png"),
+                BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT,
+                BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        Region platformerBasicsRegion = new Region();
+        platformerBasicsRegion.setPrefSize(SCENE_WIDTH+20, SCENE_HEIGHT+20);
+        platformerBasicsRegion.setBackground(new Background(myBI));
+        root.getChildren().add(platformerBasicsRegion);
+
+        //Make a Dorf!
+        Dorf ferdinand = new Dorf("sprites/BasicDorf.png", 32, 32, "Ferdinand");
+        ferdinand.setX(34);
+        ferdinand.setY(100);
+        root.getChildren().add(ferdinand.getSprite());
+
+        //Set up the controller. (Hopefully)
+        GameController controller = new GameController(ferdinand);
+        controller.initialize();
+
+        // Set up a KeyEvent handler so we can respond to keyboard activity.
+        root.setOnKeyPressed(controller);
+        root.requestFocus();
+
         primaryStage.setScene(platformerBasics);
         primaryStage.show();
     }
--- src/dorf_fortress/MainMenuController.java
+++ src/dorf_fortress/MainMenuController.java
@@ -2,12 +2,19 @@ package dorf_fortress;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
 
 /**
  * Created by azureillusions on 5/27/15.
  */
 public class MainMenuController {
+    final private double SCENE_WIDTH = 640;
+    final private double SCENE_HEIGHT = 480;
 
     public MainMenuController() {}
 
@@ -17,6 +24,35 @@ public class MainMenuController {
     @FXML
     void whenBeginClicked(ActionEvent actionEvent) {
         System.out.println("Run the damn thing.");
+        Stage eventSource = (Stage) menu_dorf_sprite.getScene().getWindow();
+
+        Group root = new Group();
+        //Make a tiled background.
+        BackgroundImage myBI = new BackgroundImage(
+                new Image("sprites/BasicTile.png"),
+                BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT,
+                BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        Region platformerBasicsRegion = new Region();
+        platformerBasicsRegion.setPrefSize(SCENE_WIDTH+20, SCENE_HEIGHT+20);
+        platformerBasicsRegion.setBackground(new Background(myBI));
+        root.getChildren().add(platformerBasicsRegion);
+
+        //Make a Dorf!
+        Dorf ferdinand = new Dorf("sprites/BasicDorf.png", 32, 32, "Ferdinand");
+        ferdinand.setX(34);
+        ferdinand.setY(100);
+        root.getChildren().add(ferdinand.getSprite());
+
+        //Set up the controller. (Hopefully)
+        GameController controller = new GameController(ferdinand);
+        controller.initialize();
+
+        // Set up a KeyEvent handler so we can respond to keyboard activity.
+        root.setOnKeyPressed(controller);
+        root.requestFocus();
+
+
+        eventSource.setScene(new Scene(root, SCENE_WIDTH, SCENE_HEIGHT));
     }
 
 }
