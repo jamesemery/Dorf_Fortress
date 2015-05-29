edc89076a725f92e437b6d05dbe8ecb79aebc4a5
Michael Stoneman <azureillusions@eduroam-171-85.dyn.carleton.edu>
2015-05-28 19:42:55 -0500

    Tweaked variables. Twerked variables as well.

--- src/dorf_fortress/Dorf.java
+++ src/dorf_fortress/Dorf.java
@@ -10,7 +10,7 @@ public class Dorf extends Actor {
     double STEP_SIZE_Y = 180/ GameController.FRAMES_PER_SECOND;
     double PLATFORM_JUMP_BOOST = 10000/ GameController.FRAMES_PER_SECOND;
     public final double FRICTION_CONSTANT = 120/ GameController.FRAMES_PER_SECOND;
-    public final double MAX_HORIZ_SPEED = 5000 / GameController.FRAMES_PER_SECOND;
+    public final double MAX_HORIZ_SPEED = 6000 / GameController.FRAMES_PER_SECOND;
     String name;
     InputBuffer inputSource;
 
