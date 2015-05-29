4ac7160d9f0903827c18f02e2dc53f9707e9fd62
Michael Stoneman <azureillusions@eduroam-171-85.dyn.carleton.edu>
2015-05-29 00:06:06 -0500

    Made a win screen and a lose screen, and controllers for Joe to work with.

---
+++ src/dorf_fortress/WinScreenController.java
@@ -0,0 +1,13 @@
+package dorf_fortress;
+
+/**
+ * Created by azureillusions on 5/29/15.
+ */
+public class WinScreenController {
+
+    @FXML
+    void whenMenuClicked(ActionEvent event) {
+
+    }
+
+}
