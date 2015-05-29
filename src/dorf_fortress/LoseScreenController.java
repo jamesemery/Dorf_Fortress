045f819b11a2ed3128a03c1d171c7cc79fccb141
Michael Stoneman <azureillusions@eduroam-171-85.dyn.carleton.edu>
2015-05-29 00:34:33 -0500

    Added useful todo statements

--- src/dorf_fortress/LoseScreenController.java
+++ src/dorf_fortress/LoseScreenController.java
@@ -1,24 +1,26 @@
 package dorf_fortress;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 /**
  * Created by azureillusions on 5/29/15.
-
+ */
 public class LoseScreenController {
 
     @FXML
     void whenMenuClicked(ActionEvent event) {
-
+        //TODO: JOE
     }
 
     @FXML
     void whenSolutionClicked(ActionEvent event) {
-
+        //TODO: JOE HELP
     }
 
     @FXML
     void whenTryAgainClicked(ActionEvent event) {
-
+        //TODO: JOE I'M BEGGING YOU
     }
 
 }
-*/
--- src/dorf_fortress/WinScreenController.java
+++ src/dorf_fortress/WinScreenController.java
@@ -1,14 +1,16 @@
 package dorf_fortress;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 /**
  * Created by azureillusions on 5/29/15.
-
+ */
 public class WinScreenController {
 
     @FXML
     void whenMenuClicked(ActionEvent event) {
-
+        // TODO: MAKE THIS
     }
 
 }
-*/
