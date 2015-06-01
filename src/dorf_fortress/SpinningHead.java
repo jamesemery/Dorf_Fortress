2f3a8d4441f226d05a4f0008e3abab3c33642dc7
jamie <emeryj@carleton.edu>
2015-06-01 00:25:43 -0500

    Added a spinning head with placeholder visuals

--- src/dorf_fortress/SpinningHead.java
+++ src/dorf_fortress/SpinningHead.java
@@ -8,44 +8,65 @@ import java.util.Random;
 public class SpinningHead extends Obstacle{
     double centerX;
     double centerY;
-    double lenth;
+    double length;
     int frameOffset;
     int rotationRate; // in frames per rotation
 
-    private SpinningHead(int hitbox_width, int hitbox_height, double x,
+    public SpinningHead(int hitbox_width, int hitbox_height, double x,
                          double y, int rate, double length, Model simulation) {
         super(hitbox_width, hitbox_height, x, y, simulation);
+        this.frameOffset = 0;
         this.rotationRate = rate;
-        this.lenth = length;
+        this.length = length;
+        centerX = x;
+        centerY = y;
     }
 
     public void setFrameOffset(int frame) {
-
+        frameOffset = frame;
     }
 
     @Override
     protected void makeSprite(double x, double y, Model simulation) {
-
+//        this.sprite = new spinningHeadSprite(x,y,this);
+        this.sprite = new Sprite("sprites/GreyDorf.png",
+                (int)this.width, (int)this.height, this);
     }
 
     @Override
     protected void makeHitbox() {
-
+        this.hitbox = new PlatformHitbox(this.width, this.height);
     }
 
     @Override
     public void collidesX(Entity projectile) {
-
+        projectile.die();
     }
 
     @Override
     public void collidesY(Entity projectile) {
-
+        projectile.die();
     }
 
+    @Override
+    public void step() {
+        int currentFrame = simulation.getCurrentFrame();
+        double angle = ((double)(currentFrame%rotationRate)/
+                (double)rotationRate) * 360;
+        double x = length*Math.cos(angle) + centerX;
+        double y = length*Math.sin(angle) + centerY;
+        this.setY(y);
+        this.setX(x);
+    }
 
     public static Obstacle getInstance(ObstaclePlacer source, Hitbox h, Random rand) {
         return null;
     }
+
+//    private class spinningHeadSprite extends Sprite {
+//        public spinningHeadSprite(double x, double y, SpinningHead spinningHead) {
+//            super();
+//        }
+//    }
 }
 
