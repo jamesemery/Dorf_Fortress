abf3d0971d9858dd2c909441951bad70fd17fa71
Michael Stoneman <azureillusions@eduroam-171-85.dyn.carleton.edu>
2015-05-31 20:22:05 -0500

    THINGS

--- src/dorf_fortress/LoseScreenController.java
+++ src/dorf_fortress/LoseScreenController.java
@@ -3,6 +3,7 @@ package dorf_fortress;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -24,15 +25,20 @@ public class LoseScreenController {
 
     @FXML
     void whenSolutionClicked(ActionEvent event) {
+        model.setGhostMode(true);
 
-
+        Stage thisStage = (Stage) primaryPane.getScene().getWindow();
+        Main.startGame(thisStage, "TODO: PASS NAME",
+                this.model.getDifficulty(), Color.MAGENTA); //TODO: CHANGE COLOR
     }
 
     @FXML
     void whenTryAgainClicked(ActionEvent event) {
         Stage thisStage = (Stage) primaryPane.getScene().getWindow();
+        Main.startGame(thisStage, "TODO: PASS NAME",
+                this.model.getDifficulty(), Color.MAGENTA); //TODO: CHANGE COLOR
         // The issue with this bit is that we don't have access to the
-        // various paramers startGame wants (i.e. beard color, difficulty,
+        // various parameters startGame wants (i.e. beard color, difficulty,
         // name). Moreover, if we just call startGame, it will (probably?)
         // generate a brand new level.
         // Main.startGame(thisStage);
