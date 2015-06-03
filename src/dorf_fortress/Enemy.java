b38f645da837712ce1e7b550d8af1f3728d53a67
Joe Adkisson <adkissonj@carleton.edu>
2015-06-02 22:38:20 -0500

    Removed unused files

--- src/dorf_fortress/Enemy.java
+++
@@ -1,9 +0,0 @@
-package dorf_fortress;
-
-/**
- * Created by Joe Adkisson on 5/24/2015.
- * Abstract subclass of Actor; moves programmatically rather then via player
- * controls.
- */
-//public class Enemy extends Actor {
-//}
--- src/dorf_fortress/Spikes.java
+++
@@ -1,8 +0,0 @@
-package dorf_fortress;
-
-/**
- * Created by Joe Adkisson on 5/24/2015.
- * Implementation of Obstacle.
- */
-//public class Spikes extends Obstacle {
-//}
