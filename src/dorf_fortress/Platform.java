15d937bc5c35dbfbab63b23a91b2815c3fa5f19c
Michael Stoneman <azureillusions@eduroam-171-85.dyn.carleton.edu>
2015-06-05 22:36:07 -0500

    Commented up a lot of code. Mostly platforms.

--- src/dorf_fortress/Platform.java
+++ src/dorf_fortress/Platform.java
@@ -1,29 +1,56 @@
 package dorf_fortress;
 
-import java.net.URL;
-
 /**
- * Created by Joe Adkisson on 5/24/2015.
- * A rectangular platform upon which a Dorf is able to stand.
+ * A rectangular surface for the Dorf or Ghost to maneuver off of. Inherits
+ * from Entity, giving it a sprite and a RectangleHitbox. Has default collision
+ * handling methods, which may be overwritten by its subclasses.
+ *
+ * Every Platform has a JUMP_BOOST constant, which contains the amount added to
+ * a Dorf or Ghost's upward velocity when they try to jump off of it. This may
+ * also be modified by the subclasses.
  */
 public abstract class Platform extends Entity {
+
     protected double JUMP_BOOST = 18000/ GameController.FRAMES_PER_SECOND;
 
-    public Platform(int hitbox_width, int
-            hitbox_height, double x, double y, Model simulation) {
+    /**
+     * The constructor for Platform. Calls the constructor for Entity and lets
+     * it handle everything.
+     * @param hitbox_width   The width of the hitbox.
+     * @param hitbox_height   The height of the hitbox.
+     * @param x   The sprite's x-coordinate on the stage.
+     * @param y   The sprite's y-coordinate on the stage.
+     * @param simulation   The singleton Model running in Main.
+     */
+    public Platform(int hitbox_width,
+                    int hitbox_height,
+                    double x,
+                    double y,
+                    Model simulation) {
         super(hitbox_width, hitbox_height, x, y, simulation);
-        this.height = hitbox_height;
-        this.width = hitbox_width;
     }
 
+    /**
+     * Constructs a RectangleHitbox with the same dimensions as the platform.
+     */
     public void makeHitbox() {
         this.hitbox = new RectangleHitbox(this.width,this.height);
     }
 
+    /**
+     * A getter method for the jump boost constant.
+     * @return JUMP_BOOST
+     */
     public double getJump() {
         return JUMP_BOOST;
     }
 
+    /**
+     * If a collision occurs from the side, this method pushes the colliding
+     * Entity back by a hair, nullifying its velocity in the process. Replaces
+     * the empty method in Entity.
+     * @param projectile   The colliding actor whose velocity is modified.
+     */
     @Override
     public void collidesX(Entity projectile) {
         // right collision - if the projectile is going faster to the right
@@ -39,6 +66,13 @@ public abstract class Platform extends Entity {
         }
     }
 
+    /**
+     * If a collision occurs from the top or bottom, this method pushes the
+     * colliding Entity back by a hair, nullifying its velocity in the process.
+     * Replaces the empty method in Entity. Allows the Dorf or Ghost to rest
+     * and walk on top of the platform.
+     * @param projectile   The colliding actor whose velocity is modified.
+     */
     @Override
     public void collidesY(Entity projectile) {
         // top collision - if the projectile is going down relative
--- src/dorf_fortress/SolidPlatform.java
+++ src/dorf_fortress/SolidPlatform.java
@@ -1,19 +1,35 @@
 package dorf_fortress;
 
 /**
- * Created by Joe Adkisson on 5/24/2015.
- * A rectangular platform upon which a Dorf is able to stand.
+ * SolidPlatform is the default variety of platform: a rectangular platform on
+ * which a Dorf is able to stand.
  */
 public class SolidPlatform extends Platform {
 
-    public SolidPlatform(int hitbox_width, int
-            hitbox_height, double x, double y, Model simulation) {
-
+    /**
+     * The constructor for SolidPlatform. Passes the information up the
+     * inheritance tree to Platform's constructor.
+     * @param hitbox_width   The width of the hitbox.
+     * @param hitbox_height   The height of the hitbox.
+     * @param x   The sprite's x-coordinate on the stage.
+     * @param y   The sprite's y-coordinate on the stage.
+     * @param simulation   The singleton Model running in Main.
+     */
+    public SolidPlatform(int hitbox_width,
+                         int hitbox_height,
+                         double x,
+                         double y,
+                         Model simulation) {
         super(hitbox_width, hitbox_height, x, y, simulation);
-        this.height = hitbox_height;
-        this.width = hitbox_width;
     }
 
+    /**
+     * Sets the platform's sprite to the default sprite
+     * @param x   The platform's x-coordinate.
+     * @param y   The platform's x-coordinate.
+     * @param simulation   The singleton Model running in Main.
+     */
+    @Override
     protected void makeSprite(double x, double y, Model simulation) {
         this.sprite = new SimpleSprite(
                 "sprites/128x32platform.png",
