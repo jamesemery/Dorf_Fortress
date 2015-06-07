f6647c2c180b1cad3d94298529b9156558398483
Joe Adkisson <adkissonj@carleton.edu>
2015-06-06 22:01:13 -0500

    SPIDERDORF, SPIDERDORF

---
+++ src/dorf_fortress/AnimatedSprite.java
@@ -0,0 +1,74 @@
+package dorf_fortress;
+
+import javafx.scene.image.ImageView;
+
+/**
+ * Created by Joe on 6/6/2015.
+ */
+public class AnimatedSprite extends Sprite {
+    private ImageView[] images;
+    private int numImages;
+    int currentImage = 0;
+    int frameDuration = 5; //how many game frames until the sprite changes state
+    int curFrame = 0;
+    boolean cycling = true;
+
+
+    public AnimatedSprite(String[] imageArray, int hitbox_width,
+                      int hitbox_height, Entity entity) {
+        super(entity);
+        this.images = new ImageView[imageArray.length];
+        for (int i = 0; i < imageArray.length; i++ ) {
+            this.images[i] = new ImageView((imageArray[i]));
+        }
+        this.numImages = this.images.length;
+        this.getChildren().add(this.images[0]);
+    }
+
+    public void update(double dorf_x) {
+        if(cycling == true) {
+            this.curFrame++;
+            if (this.curFrame > frameDuration) {
+                curFrame = 0;
+                getNextFrame();
+            }
+        }
+        // Clears the sprite and draws in the correct image.
+        this.getChildren().clear();
+            this.getChildren().add(images[this.currentImage]);
+        super.update(dorf_x);
+        super.update(dorf_x);
+    }
+
+
+    /**
+     * Sets whether the animation is cycling.
+     * @param state
+     */
+    public void setAnimationState(boolean state) {
+        this.cycling = state;
+    }
+
+    //Helper method that moves currentImage to the next index.
+    private void getNextFrame() {
+        if(this.currentImage < this.numImages -1) {
+            this.currentImage++;
+        } else {
+            this.currentImage = 0;
+        }
+    }
+
+    @Override
+    public void setX(double x) {
+        for (int i = 0; i < numImages; i++) {
+            images[i].setX(x);
+        }
+    }
+
+    @Override
+    public void setY(double y) {
+        for (int i = 0; i < numImages; i++) {
+            images[i].setY(y);
+        }
+    }
+}
