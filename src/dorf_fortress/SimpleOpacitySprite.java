f20982b63fb907f132615a99b09c498994c4894b
Michael Stoneman <azureillusions@eduroam-171-85.dyn.carleton.edu>
2015-06-08 00:42:17 -0500

    Commented more things.

--- src/dorf_fortress/SimpleOpacitySprite.java
+++ src/dorf_fortress/SimpleOpacitySprite.java
@@ -1,17 +1,29 @@
 package dorf_fortress;
 
 /**
- *
- * Created by jamie on 6/6/15.
+ * SimpleOpacitySprite is a subclass of SimpleSprite designed to handle sprites
+ * with changing opacities.
  */
 public class SimpleOpacitySprite extends SimpleSprite {
-    public SimpleOpacitySprite(String s, Entity owner) {
-        super(s, owner);
+
+    /**
+     * The constructor passes everything up the line.
+     * @param sprite_location   The location of the sprite in the repo.
+     * @param entity   The entity to which the sprite corresponds.
+     */
+    public SimpleOpacitySprite(String sprite_location, Entity entity) {
+        super(sprite_location, entity);
     }
 
+    /**
+     * Takes the Dorf's absolute x-coordinate and shifts the sprite's relative
+     * position on the screen accordingly. Also alters the sprite's opacity
+     * according to OpacityChanger.
+     * @param dorf_x   The Dorf's x-coordinate.
+     */
     @Override
     public void update(double dorf_x) {
         super.update(dorf_x);
-        image.setOpacity(((OpacityChanger)entity).getOpacity());
+        super.getImage().setOpacity(((OpacityChanger) entity).getOpacity());
     }
 }
