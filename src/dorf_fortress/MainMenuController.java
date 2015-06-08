0640a2b32e78ca5a8e6313c4c1638ef1aff5790b
Joe Adkisson <adkissonj@carleton.edu>
2015-06-08 01:20:47 -0500

    commenting

--- src/dorf_fortress/MainMenuController.java
+++ src/dorf_fortress/MainMenuController.java
@@ -1,29 +1,18 @@
 package dorf_fortress;
 
 import javafx.event.ActionEvent;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.Group;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.image.WritableImage;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import javax.swing.event.ChangeListener;
-
 /**
- * Created by azureillusions on 5/27/15.
+ * The controller class for the Main Menu.
  */
 public class MainMenuController {
     final private double SCENE_WIDTH = 640;
@@ -31,9 +20,6 @@ public class MainMenuController {
 
     public MainMenuController() {}
 
-    @FXML private AnchorPane anchorPane;
-    @FXML private Text titleText;
-    @FXML private Button beginButton;
     @FXML private CheckBox up_to_11;
     @FXML private ImageView menu_dorf_sprite;
     @FXML private Slider difficultySlider;
@@ -45,11 +31,15 @@ public class MainMenuController {
         changeMenuSprite();
     }
 
+    /**
+     * Begins the game, getting the appropriate values from the slider and
+     * color picker.
+     */
     @FXML
     void whenBeginClicked(ActionEvent actionEvent) {
         Stage thisStage = (Stage) menu_dorf_sprite.getScene().getWindow();
         if (up_to_11.isSelected()) {
-            // In our difficulty settings, 50 == 11.
+            // In our difficulty settings, "turning it to 11" means 50.
             Main.startGame(thisStage, 50, beardColorPicker.getValue());
         } else {
             Main.startGame(thisStage,
@@ -57,11 +47,12 @@ public class MainMenuController {
         }
     }
 
+    /**
+     * Changes the Dorf sprite on the Main Menu to match the color selected.
+     */
     public void changeMenuSprite() {
         Image newImage = new Image("sprites/ColoredDorf.png");
-        System.out.println(newImage);
         newImage = Main.colorImage(newImage, beardColorPicker.getValue());
-        System.out.println(newImage);
         menu_dorf_sprite.setImage(newImage);
     }
 }
